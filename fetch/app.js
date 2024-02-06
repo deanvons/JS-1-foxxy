7cb32f6819c37aa33c1dbb1ca0e3cb4ca71461ae
LAPTOP-5RU3UB56\Berke <berken_93@hotmail.com>
2024-02-06 12:10:59 +0100

    Some last code that I forgot to push last time.

--- fetch/app.js
+++ fetch/app.js
@@ -1,20 +1,58 @@
-// data
 // you will be working on arrays and objects mostly
 
 // data
 let todos = []
 
-let todo = fetch('https://jsonplaceholder.typicode.com/users')
+let todo = fetch('https://jsonplaceholder.typicode.com/todos')
 .then(function (httpResponse) {
     console.log(httpResponse);
     //console.log(httpResponse)
-    // extract the data
+    // extract the data (using the json function)
     return httpResponse.json()
 })
 .then(function(todoResult) {
+    // do something with the data (store it in the todos variable)
     todos = todoResult;
 });
 
-console.log("other stuff");
-
 // Show it to the user
+
+//post request. Still gonna use fetch. 
+// delete request. Asks the api to delete the data thats not in use.
+
+fetch('https://jsonplaceholder.typicode.com/posts', {
+    method: 'POST',
+    body: JSON.stringify({
+    title: 'foo',
+    body: 'bar',
+    userId: 1,
+    }),
+    headers: {
+    'Content-type': 'application/json; charset=UTF-8',
+    },
+})
+    .then((response) => response.json())
+    .then((json) => console.log(json));
+
+fetch("https://jsonplaceholder.typicode.com/todos")
+.then(function (httpResponse){
+})
+
+// go fetch I promise to forward to. then
+
+// await going to fetch
+// forward to the next line
+
+
+fetch("https://jsonplaceholder.typicode.com/todos")
+    .then(function (httpResponse){
+        console.log(httpResponse)
+        return httpResponse.json();
+    })
+    .then((data) => console.log(data));
+
+    async function getTodos(){
+        let httpResponse = await fetch("https://jsonplaceholder.typicode.com/todos")
+        let data = await httpResponse.json()
+        console.log(data);
+    }
